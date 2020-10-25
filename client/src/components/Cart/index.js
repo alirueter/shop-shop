d6bf0c99cba12177360d3c3233c3c31a42adda3e
alruet <rueter.alison@gmail.com>
2020-10-25 17:50:56 -0500

    working on cart/index.js redux

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,20 +8,18 @@ import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 // const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     // const [state, dispatch] = useStoreContext();
-    
-    // REDUX - ??? 
-    // const cartLength = useSelector( state => {
-    //   state.cart.length
-    // })
 
     const dispatch = useDispatch();
+    const state = useSelector(state => state);
 
+  
     const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
     const [getCheckout, { data }] =useLazyQuery(QUERY_CHECKOUT);
